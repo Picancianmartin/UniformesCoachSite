6c269faa883ffac62d76d9a06481bdf55fc1ded5
Pietra <martincpietra@icloud.com>
2026-02-13 06:51:25 -0300

    mudando servidor para sa-east-1 v1.0

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Pegue esses dados no Painel do Supabase > Project Settings > API
-const supabaseUrl = 'https://vpnctsilwzxpgooewvrg.supabase.co' // ex: https://xyz.supabase.co
-const supabaseKey = 'sb_publishable_Tvqz2Zq39tUS5vPYwn5F9A_9j-6EEbI'        // A chave pública (public/anon)
+const supabaseUrl = 'https://khabhzzigvuyuljiruce.supabase.co' // ex: https://xyz.supabase.co
+const supabaseKey = 'sb_publishable_ZAKthjkahy5f8SFEi8eybA_8g9phYeT'        // A chave pública (public/anon)
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
